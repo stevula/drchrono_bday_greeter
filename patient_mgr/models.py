c2aa22339b99e29c337dda6f6c6c5626ed6528a8
stevula <brodericksteven@gmail.com>
2016-07-01 11:30:38 -0700

    Reinitialize git repo ( (oops, lost commit history while moving dir)

---
+++ patient_mgr/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+import datetime
+from django.db import models
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    dob = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return "%s (%s)" % (self.name, self.dob)
+
+    def age(self):
+        return datetime.datetime.now().year - self.dob
---
+++ patient_mgr/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, HttpResponse
+from django.template import loader
+
+from.models import Patient
+
+
+def welcome(request):
+    return HttpResponse("Welcome!")
+
+
+def index(request):
+    patients = Patient.objects.all()
+    return render(request, 'patient_mgr/index.html', {'patient_list': patients})
+
+
+def show(request, patient_id):
+    try:
+        patient = Patient.objects.get(pk=patient_id)
+    except Patient.DoesNotExist:
+        raise Http404("Patient does not exist")
+    return render(request, 'patient_mgr/show.html', {'patient': patient})
+
+
+def new(request):
+    return HttpResponse("new")
