841ad184f7db7f4ef20d95100705ff46d545835a
stevula <brodericksteven@gmail.com>
2016-06-30 18:01:22 -0700

    Add Doctor models and fields to Doctor/Patient

--- drchrono_bday/happy_bday/models.py
+++ drchrono_bday/happy_bday/models.py
@@ -3,5 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class User(models.Model):
+class Doctor(models.model):
     name = models.CharField(max_length=200)
+
+
+class Patient(models.Model):
+    doctor = models.ForeignKey(QUestion, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    dob = models.DateTimeField
