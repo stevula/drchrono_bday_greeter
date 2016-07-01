35337646dc8e3194c88f0c8e3dca14c6d1db90cc
stevula <brodericksteven@gmail.com>
2016-07-01 10:23:47 -0700

    Add more patient routes

--- drchrono_bday/happy_bday/urls.py
+++ drchrono_bday/happy_bday/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.welcome, name='welcome'),
+    url(r'^patient/index/$', views.index, name='index'),
+    url(r'^patient/new/$', views.new, name='new'),
+    url(r'^patient/(?P<patient_id>[0-9]+)/$', views.show, name='show'),
 ]
--- drchrono_bday/happy_bday/views.py
+++ drchrono_bday/happy_bday/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
 
+def welcome(request):
+    return HttpResponse("Welcome!")
+
+
 def index(request):
-    return HttpResponse("happy_bday index")
+    return HttpResponse("Index")
+
+
+def new(request):
+    return HttpResponse("new")
+
+
+def show(request, patient_id):
+    return HttpResponse("show %s" % patient_id)
