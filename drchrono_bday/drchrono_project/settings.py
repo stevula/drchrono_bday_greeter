89cb8c404b731142d7fd6ceaed1e105476e4b20a
stevula <brodericksteven@gmail.com>
2016-06-30 18:06:33 -0700

    Create model migrations

--- drchrono_bday/drchrono_project/settings.py
+++ drchrono_bday/drchrono_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'happy_bday.apps.HappyBdayConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- drchrono_bday/happy_bday/models.py
+++ drchrono_bday/happy_bday/models.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Doctor(models.model):
+class Doctor(models.Model):
     name = models.CharField(max_length=200)
 
 
 class Patient(models.Model):
-    doctor = models.ForeignKey(QUestion, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     dob = models.DateTimeField
