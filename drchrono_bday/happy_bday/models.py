99327d973572b21e8f299ada1da0848a5521f0c0
stevula <brodericksteven@gmail.com>
2016-06-30 18:20:49 -0700

    Add models to admin interface

--- drchrono_bday/happy_bday/models.py
+++ drchrono_bday/happy_bday/models.py
@@ -1,13 +1,23 @@
 from __future__ import unicode_literals
 
+import datetime
 from django.db import models
 
 
 class Doctor(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return {name: self.name}
+
 
 class Patient(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     dob = models.DateTimeField
+
+    def __str__(self):
+        return {name: self.name, dob: self.dob}
+
+    def age(self):
+        return datetime.year - self.dob.year
