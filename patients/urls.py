1ea49391f73cc9863eef82e63932c9f026b6a631
stevula <brodericksteven@gmail.com>
2016-07-07 17:12:50 -0700

    Change to class-based views

--- patients/urls.py
+++ patients/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^patients/$', views.index, name='index'),
-    url(r'^patients/(?P<patient_id>[0-9]+)/$', views.show, name='show'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^patients/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,31 +1,35 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from.models import Patient, Doctor
 
 
-def index(request):
-    # TODO: get currently signed in doctor
-    doctor = Doctor.objects.get(pk=1)
+class IndexView(generic.ListView):
+    template_name = 'patients/index.html'
+    context_object_name = 'patient_list'
+
+    def get_queryset(self):
+        """ All patients for currently signed in doctor """
+        # TODO: get currently signed in doctor
+        doctor = Doctor.objects.get(pk=1)
+        return doctor.patient_set.all()
 
     # TODO: fetch patient data from api instead of form
-    if request.method == 'POST':
-        form = request.POST
-        # create Patient
-        doctor.patient_set.create(
-            name=form['name'],
-            dob=form['dob'],
-            email=form['email'],
-            phone=form['phone'])
-        return HttpResponseRedirect(reverse('patients:index'))
-    else:
-        # TODO: get patients belonging to this doctor only
-        patients = doctor.patient_set.all()
-        return render(request, 'patients/index.html', {
-            'patient_list': patients, 'doctor': doctor})
+    # if request.method == 'POST':
+    #     form = request.POST
+    #     # create Patient
+    #     doctor.patient_set.create(
+    #         name=form['name'],
+    #         dob=form['dob'],
+    #         email=form['email'],
+    #         phone=form['phone'])
+    #     return HttpResponseRedirect(reverse('patients:index'))
 
 
-def show(request, patient_id):
-    patient = get_object_or_404(Patient, pk=patient_id)
-    return render(request, 'patients/show.html', {'patient': patient})
+class DetailView(generic.DetailView):
+    model = Patient
+    template_name = 'patients/detail.html'
+    # patient = get_object_or_404(Patient, pk=patient_id)
+    # return render(request, 'patients/show.html', {'patient': patient})
