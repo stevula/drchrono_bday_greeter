3ee3c0454de0f2abb9bcf8d5a3efae335ec95790
stevula <brodericksteven@gmail.com>
2016-07-07 17:46:18 -0700

    Change filenames to match convention

--- patients/views.py
+++ patients/views.py
@@ -8,32 +8,15 @@ from.models import Patient, Doctor
 
 
 class IndexView(generic.ListView):
-    template_name = 'patients/index.html'
-    context_object_name = 'patient_list'
-
     def get_queryset(self):
         """ All patients for currently signed in doctor """
         # TODO: get currently signed in doctor
         doctor = Doctor.objects.get(pk=1)
         return doctor.patient_set.all()
 
-    # TODO: fetch patient data from api instead of form
-    # if request.method == 'POST':
-    #     form = request.POST
-    #     # create Patient
-    #     doctor.patient_set.create(
-    #         name=form['name'],
-    #         dob=form['dob'],
-    #         email=form['email'],
-    #         phone=form['phone'])
-    #     return HttpResponseRedirect(reverse('patients:index'))
-
 
 class DetailView(generic.DetailView):
     model = Patient
-    template_name = 'patients/detail.html'
-    # patient = get_object_or_404(Patient, pk=patient_id)
-    # return render(request, 'patients/show.html', {'patient': patient})
 
 
 def add(request):
