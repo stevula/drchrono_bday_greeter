2b111347bd79a9a83009a550675afc35bf8e6e60
stevula <brodericksteven@gmail.com>
2016-07-07 20:09:28 -0700

    Add STATIC_ROOT

--- patients/urls.py
+++ patients/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 
+# from happy_bday import settings
 from . import views
 
 urlpatterns = [
@@ -7,5 +8,6 @@ urlpatterns = [
     url(r'^patients/$', views.IndexView.as_view(), name='index'),
     url(r'^patients/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^patients/create/$', views.create, name='create'),
-    url(r'^patients/destroy/$', views.destroy, name='destroy')
+    url(r'^patients/destroy/$', views.destroy, name='destroy'),
+    # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 ]
